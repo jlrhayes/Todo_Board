cb80e5109df507b6c395589597021009c0b29ae7
Josh Hayes <hays@fb.com>
2021-10-11 14:35:20 +0100

    Add columns

--- todo_client/src/App.js
+++ todo_client/src/App.js
@@ -1,25 +1,16 @@
 import Board from './components/Board';
+import Login from './components/Login';
+import Register from './components/Register';
+
 import {useState} from 'react'
 import './App.css';
 
 function App() {
 
-  //need to move this into board dashboard
-  const [columns, setColumns] = useState([
-    {
-      id : 1,
-      name : "Todo",
-    },
-    {
-      id : 2,
-      name : 'In progress',
-    }
-  ])
-
 
   return (
     <div className="App">
-      <Board columns = {columns}></Board>
+      <Board></Board>
     </div>
   );
 }
--- todo_client/src/components/Column.js
+++ todo_client/src/components/Column.js
@@ -4,20 +4,14 @@ import { useState } from "react"
 
 const Column = ({column}) => {
     //need to find tasks under column id and add to task list
-    const [tasks,setTasks] = useState([
-        {
-            id : 1,
-            name : 'First Task',
-            description : 'My first task ever'
-        }
-
-    ])
+    const [tasks,setTasks] = useState([])
 
     return (
         <div>
             <header>{column.name}</header>
-                {tasks.map((task) => (<Task className = "task" key = {task.id} task = {task}/>))}
+            {tasks.map((task) => (<Task className = "task" key = {task.id} task = {task}/>))}
         </div>
+        
     )
 }
 
