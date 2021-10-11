4bd05b0b90f6240fcfb51eef8c59991070a55b20
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-10-11 13:43:01 +0100

    christie's app.js for login

--- todo_client/src/App.js
+++ todo_client/src/App.js
@@ -1,14 +1,38 @@
-
-import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Todo App
-      </header>
-    </div>
-  );
+    const [userId, setUserId] = useState();
+
+    if (!userId) {
+        return <BrowserRouter>
+            <Switch>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
+            </Switch>
+        </BrowserRouter>;
+    }
+
+    return (
+        <div className="wrapper">
+            <h1>To-Do App</h1>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
