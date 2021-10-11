5de357e57304af6ce54315267ba92444b0236191
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-10-11 13:55:05 +0100

    Combining login & column app.js

--- todo_client/src/App.js
+++ todo_client/src/App.js
@@ -1,36 +1,43 @@
 import { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Board from "./components/Board";
 
 function App() {
     const [userId, setUserId] = useState();
 
     if (!userId) {
-        return <BrowserRouter>
-            <Switch>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route path="/register">
-                    <Register />
-                </Route>
-            </Switch>
-        </BrowserRouter>;
-    }
-
-    return (
-        <div className="wrapper">
-            <h1>To-Do App</h1>
+        return (
             <BrowserRouter>
                 <Switch>
-                    <Route path="/dashboard">
-                        <Dashboard />
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/register">
+                        <Register />
                     </Route>
                 </Switch>
             </BrowserRouter>
+        );
+    }
+
+    //need to move this into board dashboard
+    const [columns, setColumns] = useState([
+        {
+            id: 1,
+            name: "Todo",
+        },
+        {
+            id: 2,
+            name: "In progress",
+        },
+    ]);
+
+    return (
+        <div className="App">
+            <Board columns={columns}></Board>
         </div>
     );
 }
