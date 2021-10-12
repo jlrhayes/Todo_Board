7a3ed44007f98661bb8304b3e60ca5f92726ed6f
JackLillie <42320657+JackLillie@users.noreply.github.com>
2021-10-12 13:14:12 +0100

    Ignore sqlite file + add cascading delete

--- todo_backend/setupDb.js
+++ todo_backend/setupDb.js
@@ -5,16 +5,16 @@ const Task = require("./task");
 const db = require("./db");
 
 async function setupDb() {
-  User.hasMany(Board);
+  User.hasMany(Board, { onDelete: "cascade" });
   Board.belongsTo(User);
 
-  User.hasMany(Task);
+  User.hasMany(Task, { onDelete: "cascade" });
   Task.belongsTo(User);
 
-  Board.hasMany(Column);
+  Board.hasMany(Column, { onDelete: "cascade" });
   Column.belongsTo(Board);
 
-  Column.hasMany(Task);
+  Column.hasMany(Task, { onDelete: "cascade" });
   Task.belongsTo(Column);
 
   await db.sync({ force: process.env.NODE_ENV === "test" });
