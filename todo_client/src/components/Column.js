d2ec0886ce5ab52c776917ed61b4aebaff309cd9
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-10-14 12:18:37 +0100

    showing edit for column on edit

--- todo_client/src/components/Column.js
+++ todo_client/src/components/Column.js
@@ -3,14 +3,16 @@ import AddTask from "./AddTask";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import React, { useState } from "react";
+import AddColumn from "./AddColumn";
 
-const Column = ({ column , onDelete}) => {
+const Column = ({ column, onDelete, onEdit }) => {
     //need to find tasks under column id and add to task list
     const [tasks, setTasks] = useState([]);
+    const [edit, setShowEdit] = useState(false);
 
     React.useEffect(() => {
-        getTasks()
-      }, []);
+        getTasks();
+    }, []);
 
     const addTask = async (newTask) => {
         newTask["columnId"] = column.id;
@@ -19,10 +21,7 @@ const Column = ({ column , onDelete}) => {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(newTask),
         };
-        await fetch(
-            `http://localhost:4000/tasks`,
-            requestOptions
-        );
+        await fetch(`http://localhost:4000/tasks`, requestOptions);
         getTasks();
     };
 
@@ -35,39 +34,55 @@ const Column = ({ column , onDelete}) => {
 
     const deleteTask = async (id) => {
         const requestOptions = { method: "DELETE" };
-        await fetch(
-            `http://localhost:4000/tasks/${id}`,
-            requestOptions
-        );
+        await fetch(`http://localhost:4000/tasks/${id}`, requestOptions);
         getTasks();
     };
 
-    const editColumn = () => {
-        console.log("edited");
+    const editColumn = async (updatedColumn) => {
+        console.log(updatedColumn);
+        const requestOptions = {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updatedColumn),
+        };
+        await fetch(
+            `http://localhost:4000/columns/${column.id}`,
+            requestOptions
+        );
+        onEdit();
+        setShowEdit(false);
     };
 
     return (
         <div className="w-96 mx-8 justify-self-center">
             <div className="spartan border-red-50 flex justify-between items-center">
-                <header>{column.title}</header>
+                {edit ? (
+                    <AddColumn
+                        onAdd={editColumn}
+                        placeholder="Column Name"
+                    ></AddColumn>
+                ) : (
+                    <header>{column.title}</header>
+                )}
                 <div className="flex justify-between">
-                    <button
-                        className="delete-button submit"
-                        onClick={onDelete}
-                    >
+                    <button className="delete-button submit" onClick={onDelete}>
                         <DeleteIcon fontSize="small" />
                     </button>
                     <button
                         className="delete-button submit"
-                        onClick={editColumn}
+                        onClick={() => setShowEdit(true)}
                     >
                         <EditIcon fontSize="small" />
                     </button>
                 </div>
             </div>
             {tasks.map((task) => (
-                <Task className="task" key={task.id} task={task} 
-                deleteTask={() => deleteTask(task.id)} />
+                <Task
+                    className="task"
+                    key={task.id}
+                    task={task}
+                    deleteTask={() => deleteTask(task.id)}
+                />
             ))}
             <AddTask onAdd={addTask}></AddTask>
         </div>
