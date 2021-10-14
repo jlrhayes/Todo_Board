7414686bc742f68695c14c9a15fdf3cf9d8d076f
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2021-10-14 13:09:32 +0100

    enabled editing task

--- todo_client/src/components/Task.js
+++ todo_client/src/components/Task.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import "./Task.css";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
+import AddTask from "./AddTask";
 
-const Task = ({ task, deleteTask }) => {
+const Task = ({ task, deleteTask, onEdit }) => {
     //need to find tasks under column id and add to task list
     const [user, setUser] = useState([]);
+    const [edit, setShowEdit] = useState(false);
 
     React.useEffect(() => {
         getUser();
@@ -13,31 +15,51 @@ const Task = ({ task, deleteTask }) => {
 
     const getUser = () => {
         fetch(`http://localhost:4000/users/${task.userId}`)
-          .then((res) => res.json())
-          .then((data) => setUser(data))
+            .then((res) => res.json())
+            .then((data) => setUser(data));
+    };
+
+    const editTask = async (editedTask) => {
+        const requestOptions = {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(editedTask),
+        };
+        await fetch(
+            `http://localhost:4000/tasks/${task.id}`,
+            requestOptions
+        );
+        onEdit();
+        setShowEdit(false);
     };
 
     return (
         <div className="task">
+            {edit ? (
+                <AddTask onAdd={editTask} heading={"Edit Task"} />
+            ) : (
+                <TaskDetails task={task} user={user} />
+            )}
+            <div className="flex flex-row justify-end">
+                <button className="delete-button submit" onClick={deleteTask}>
+                    <DeleteIcon fontSize="small" />
+                </button>
+                <button className="delete-button submit" onClick={() => setShowEdit(!edit)}>
+                    <EditIcon fontSize="small" />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+const TaskDetails = ({ task, user }) => {
+    return (
+        <div className="flex flex-col">
             <h3 className="font-bold text-lg">{task.title}</h3>
             <h4>{task.description}</h4>
             <div className="mx-0 flex flex-row items-center">
-                <img className="w-8" src={user.avatarUrl}/>
+                <img className="w-8" src={user.avatarUrl} />
                 {user.name}
-            </div>
-            <div className="flex flex-row justify-end">
-                <button
-                    className="delete-button submit"
-                    onClick={deleteTask}
-                >
-                    <DeleteIcon fontSize="small" />
-                </button>
-                {/*<button
-                    className="delete-button submit"
-                    onClick={editTask(task.id)}
-                >
-                    <EditIcon fontSize="small" />
-                </button>*/}
             </div>
         </div>
     );
